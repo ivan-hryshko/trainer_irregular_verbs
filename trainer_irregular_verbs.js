0ffbe035bc00a395892d51085dc5f65bc1fb8c45
giver0 <giver313@gmail.com>
2022-04-29 22:29:46 +0300

    added words to dictionary

--- trainer_irregular_verbs.js
+++ trainer_irregular_verbs.js
@@ -2,7 +2,22 @@ console.log("Hello")
 
 const dictionaty = [
   {
-    present: 'go'
+    present: 'go',
+    past: 'went',
+    particle: 'gone',
+    urkaine: 'Йти'
   },
-  
+  {
+    present: 'do',
+    past: 'did',
+    particle: 'done',
+    urkaine: 'робити'
+  },
+  {
+    present: 'eat',
+    past: 'ate',
+    particle: 'eaten',
+    urkaine: 'їсти'
+  },
+
 ]
