7c9c0eaa76379cdd6c289604aea91d35150213e7
giver0 <giver313@gmail.com>
2022-04-30 18:18:44 +0300

    add keyListener

--- trainer_irregular_verbs.js
+++ trainer_irregular_verbs.js
@@ -1,6 +1,4 @@
-console.log("Hello")
-
-const dictionaty = [
+const dictionary = [
   {
     present: 'go',
     past: 'went',
@@ -21,3 +19,48 @@ const dictionaty = [
   },
 
 ]
+
+word = document.querySelector('.trainer__layout-trainer-word')
+myAnswer = document.querySelector('.trainer__layout-trainer-answer')
+
+
+console.log("Hello")
+
+// answer.addEventListener('input', (event) => {
+  //   // console.log(event.target.value)
+  //   //
+    // });
+// }
+
+let isAnswerCorrect = 'true'
+
+for (const word of dictionary) {
+  isAnswerCorrect = 'true'
+  word.innerText = word.present
+  while (isAnswerCorrect) {
+
+
+  }
+
+}
+
+document.addEventListener('keyup', logKey);
+function logKey(e) {
+  if(e.code === 'Enter'){
+    checkAnswer()
+  }
+}
+
+function checkAnswer() {
+  // console.log(answer)
+  console.log(answer.value)
+  if (myAnswer.value === answer) {
+    isAnswerCorrect = 'false'
+
+  }
+  // console.log(answer.event.target.value)
+
+}
+
+
+
