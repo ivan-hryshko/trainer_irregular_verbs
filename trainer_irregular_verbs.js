047ca9e8fa735850ea4450bb0a6449b890f85833
giver0 <giver313@gmail.com>
2022-05-08 08:38:06 +0300

    create vue projects

--- trainer_irregular_verbs.js
+++ trainer_irregular_verbs.js
@@ -1,431 +1,39 @@
-const dictionary = [
-  {
-    present: 'go',
-    past: 'went',
-    participle: 'gone',
-    ukraine: 'Йти'
-  },
-  {
-    present: 'do',
-    past: 'did',
-    participle: 'done',
-    ukraine: 'робити'
-  },
-  {
-    present: 'eat',
-    past: 'ate',
-    participle: 'eaten',
-    ukraine: 'їсти'
-  },
-  {
-    present: 'become',
-    past: 'became',
-    participle: 'become',
-    ukraine: ''
-  },
-  {
-    present: 'begin',
-    past: 'began',
-    participle: 'begun',
-    ukraine: ''
-  },
-  {
-    present: 'break',
-    past: 'broke',
-    participle: 'broken',
-    ukraine: ''
-  },
-  {
-    present: 'bring',
-    past: 'brought',
-    participle: 'brought',
-    ukraine: ''
-  },
-  {
-    present: 'build',
-    past: 'built',
-    participle: 'built',
-    ukraine: ''
-  },
-  {
-    present: 'buy',
-    past: 'bought',
-    participle: 'bought',
-    ukraine: ''
-  },
-  {
-    present: 'catch',
-    past: 'caught',
-    participle: 'caught',
-    ukraine: ''
-  },
-  {
-    present: 'choose',
-    past: 'chose',
-    participle: 'chosen',
-    ukraine: ''
-  },
-  {
-    present: 'come',
-    past: 'came',
-    participle: 'come',
-    ukraine: ''
-  },
-  {
-    present: 'cost',
-    past: 'cost',
-    participle: 'cost',
-    ukraine: ''
-  },
-  {
-    present: 'cut',
-    past: 'cut',
-    participle: 'cut',
-    ukraine: ''
-  },
-  {
-    present: 'drink',
-    past: 'drank',
-    participle: 'drunk',
-    ukraine: ''
-  },
-  {
-    present: 'drive',
-    past: 'drove',
-    participle: 'driven',
-    ukraine: ''
-  },
-  {
-    present: 'fall',
-    past: 'fell',
-    participle: 'fallen',
-    ukraine: ''
-  },
-  {
-    present: 'fall',
-    past: 'fell',
-    participle: 'fallen',
-    ukraine: ''
-  },
-  {
-    present: 'feel',
-    past: 'felt',
-    participle: 'felt',
-    ukraine: ''
-  },
-  {
-    present: 'find',
-    past: 'found',
-    participle: 'found',
-    ukraine: ''
-  },
-  {
-    present: 'fly',
-    past: 'flew',
-    participle: 'flown',
-    ukraine: ''
-  },
-  {
-    present: 'forget',
-    past: 'forgot',
-    participle: 'forgotten',
-    ukraine: ''
-  },
-  {
-    present: 'get',
-    past: 'got',
-    participle: 'got',
-    ukraine: ''
-  },
-  {
-    present: 'give',
-    past: 'gave',
-    participle: 'given',
-    ukraine: ''
-  },
-  {
-    present: 'grow',
-    past: 'grew',
-    participle: 'grown',
-    ukraine: ''
-  },
-  {
-    present: 'have',
-    past: 'had',
-    participle: 'had',
-    ukraine: ''
-  },
-  {
-    present: 'hear',
-    past: 'heard',
-    participle: 'heard',
-    ukraine: ''
-  },
-  {
-    present: 'hit',
-    past: 'hit',
-    participle: 'hit',
-    ukraine: ''
-  },
-  {
-    present: 'keep',
-    past: 'kept',
-    participle: 'kept',
-    ukraine: ''
-  },
-  {
-    present: 'know',
-    past: 'knew',
-    participle: 'known',
-    ukraine: ''
-  },
-  {
-    present: 'learn',
-    past: 'learnt',
-    participle: 'learnt',
-    ukraine: ''
-  },
-  {
-    present: 'leave',
-    past: 'left',
-    participle: 'left',
-    ukraine: ''
-  },
-  {
-    present: 'lend',
-    past: 'lent',
-    participle: 'lent',
-    ukraine: ''
-  },
-  {
-    present: 'let',
-    past: 'let',
-    participle: 'let',
-    ukraine: ''
-  },
-  {
-    present: 'lose',
-    past: 'lost',
-    participle: 'lost',
-    ukraine: ''
-  },
-  {
-    present: 'make',
-    past: 'made',
-    participle: 'made',
-    ukraine: ''
-  },
-  {
-    present: 'meet',
-    past: 'met',
-    participle: 'met',
-    ukraine: ''
-  },
-  {
-    present: 'pay',
-    past: 'paid',
-    participle: 'paid',
-    ukraine: ''
-  },
-  {
-    present: 'put',
-    past: 'put',
-    participle: 'put',
-    ukraine: ''
-  },
-  {
-    present: 'read',
-    past: 'read',
-    participle: 'read',
-    ukraine: ''
-  },
-  {
-    present: 'ring',
-    past: 'rang',
-    participle: 'rung',
-    ukraine: ''
-  },
-  {
-    present: 'run',
-    past: 'ran',
-    participle: 'run',
-    ukraine: ''
-  },
-  {
-    present: 'say',
-    past: 'said',
-    participle: 'said',
-    ukraine: ''
-  },
-  {
-    present: 'see',
-    past: 'saw',
-    participle: 'seen',
-    ukraine: ''
-  },
-  {
-    present: 'sell',
-    past: 'sold',
-    participle: 'sold',
-    ukraine: ''
-  },
-  {
-    present: 'send',
-    past: 'sent',
-    participle: 'sent',
-    ukraine: ''
-  },
-  {
-    present: 'shut',
-    past: 'shut',
-    participle: 'shut',
-    ukraine: ''
-  },
-  {
-    present: 'sing',
-    past: 'sang',
-    participle: 'sung',
-    ukraine: ''
-  },
-  {
-    present: 'sit',
-    past: 'sat',
-    participle: 'sat',
-    ukraine: ''
-  },
-  {
-    present: 'sleep',
-    past: 'slept',
-    participle: 'slept',
-    ukraine: ''
-  },
-  {
-    present: 'speak',
-    past: 'spoke',
-    participle: 'spoken',
-    ukraine: ''
-  },
-  {
-    present: 'spend',
-    past: 'spent',
-    participle: 'spent',
-    ukraine: ''
-  },
-  {
-    present: 'stand',
-    past: 'stood',
-    participle: 'stood',
-    ukraine: ''
-  },
-  {
-    present: 'steal',
-    past: 'stole',
-    participle: 'stolen',
-    ukraine: ''
-  },
-  {
-    present: 'swim',
-    past: 'swam',
-    participle: 'swum',
-    ukraine: ''
-  },
-  {
-    present: 'take',
-    past: 'took',
-    participle: 'taken',
-    ukraine: ''
-  },
-  {
-    present: 'teach',
-    past: 'taught',
-    participle: 'taught',
-    ukraine: ''
-  },
-  {
-    present: 'tell',
-    past: 'told',
-    participle: 'told',
-    ukraine: ''
-  },
-  {
-    present: 'think',
-    past: 'thought',
-    participle: 'thought',
-    ukraine: ''
-  },
-  {
-    present: 'throw',
-    past: 'threw',
-    participle: 'thrown',
-    ukraine: ''
-  },
-  {
-    present: 'understand',
-    past: 'understood',
-    participle: 'understood',
-    ukraine: ''
-  },
-  {
-    present: 'wake',
-    past: 'woke',
-    participle: 'woken',
-    ukraine: ''
-  },
-  {
-    present: 'wear',
-    past: 'wore',
-    participle: 'worn',
-    ukraine: ''
-  },
-  {
-    present: 'win',
-    past: 'won',
-    participle: 'won',
-    ukraine: ''
-  },
-  {
-    present: 'write',
-    past: 'wrote',
-    participle: 'written',
-    ukraine: ''
-  },
-]
+import * as fs from 'fs';
 
-const word = document.querySelector('.trainer__layout-trainer-word')
-const myAnswer = document.querySelector('.trainer__layout-trainer-input')
-const error = document.querySelector('.trainer__layout-trainer-answer')
-const wordCounter = document.querySelector('.trainer__layout-trainer-counter')
-const devField = document.querySelector('.trainer__layout-trainer-dev_field')
-let currentWord = 0
-let isAnswerCorrect = 'true'
-let answer = 'dictionary[wordCounter].participle'
-let randomWordArray = dictionary.slice()
-let mistakeArray = []
-let mistakeCounter = 0
+const wordsArray = fs.readFileSync('./trainer_irregular_verbs.json');
+console.log(wordsArray);
+const word = document.querySelector('.trainer__layout-trainer-word');
+const myAnswer = document.querySelector('.trainer__layout-trainer-input');
+const error = document.querySelector('.trainer__layout-trainer-answer');
+const wordCounter = document.querySelector('.trainer__layout-trainer-counter');
+const devField = document.querySelector('.trainer__layout-trainer-dev_field');
+let currentWord = 0;
+let isAnswerCorrect = 'true';
+let answer = 'dictionary[wordCounter].participle';
+let randomWordArray = wordsArray.slice();
+let mistakeArray = [];
+let mistakeCounter = 0;
 
-console.log("Hello")
+console.log('Hello');
 
-start()
+start();
 
 function start() {
-  randomizeArray()
-  checkRepeat()
+  randomizeArray();
+  checkRepeat();
   // console.log(randomWordArray)
   // console.log(dictionary)
-  changeWord()
+  changeWord();
 }
 
 function randomizeArray() {
-  randomWordArray.sort(function(){
-    return Math.random() - 0.5;
-  });
+  randomWordArray.sort(() => Math.random() - 0.5);
 }
 
 function changeWord() {
-  word.innerText = randomWordArray[currentWord].present
-  answer = randomWordArray[currentWord].participle
-  wordCounter.innerText = `${currentWord + 1} of ${randomWordArray.length}`
-
+  word.innerText = randomWordArray[currentWord].present;
+  answer = randomWordArray[currentWord].participle;
+  wordCounter.innerText = `${currentWord + 1} of ${randomWordArray.length}`;
 }
 
 // document.addEventListener('keyup', logKey);
@@ -439,102 +47,92 @@ function changeWord() {
 
 function enterKeyPressed(event) {
   if (event.keyCode === 13) {
-    console.log("Enter key is pressed");
-    checkAnswer()
+    console.log('Enter key is pressed');
+    checkAnswer();
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 function checkAnswer() {
-  console.log(myAnswer.value)
-  console.log(answer)
+  console.log(myAnswer.value);
+  console.log(answer);
   if (myAnswer.value.toLowerCase() === answer) {
-    isAnswerCorrect = 'false'
-    correctAnswer()
+    isAnswerCorrect = 'false';
+    correctAnswer();
   } else {
-    wrongAnswer()
-
+    wrongAnswer();
   }
   // console.log(answer.event.target.value)
-
 }
 
 function sleep(ms) {
   return new Promise((resolve) => {
-    setTimeout(resolve, ms)
-  })
+    setTimeout(resolve, ms);
+  });
 }
 
 async function correctAnswer() {
-  console.log("Correct")
-  error.innerText = 'Correct!'
-  error.style.color = "green";
-  await sleep(1000)
-  error.innerText = ''
-  myAnswer.value = ''
-  checkEndGame()
+  console.log('Correct');
+  error.innerText = 'Correct!';
+  error.style.color = 'green';
+  await sleep(1000);
+  error.innerText = '';
+  myAnswer.value = '';
+  checkEndGame();
 }
 
 async function wrongAnswer() {
-  console.log("wrong")
-  error.innerText = answer
+  console.log('wrong');
+  error.innerText = answer;
   error.style.color = 'red';
-  mistakeCounter += 1
-  await sleep(2000)
-  myAnswer.value = ''
-  error.innerText = ''
+  mistakeCounter += 1;
+  await sleep(2000);
+  myAnswer.value = '';
+  error.innerText = '';
 
-  mistakeArray.push(randomWordArray[currentWord])
+  mistakeArray.push(randomWordArray[currentWord]);
 
-  checkEndGame()
-
-
+  checkEndGame();
 }
 
 function checkEndGame() {
-  currentWord += 1
+  currentWord += 1;
   if (currentWord < randomWordArray.length) {
-
-    changeWord()
-
+    changeWord();
   } else {
-    let correctAnswerCount = ((randomWordArray.length - mistakeCounter) * 100) / randomWordArray.length
-    correctAnswerCount = Math.floor(correctAnswerCount)
-    alert (`Mistake: ${mistakeCounter}\nCorrect: ${correctAnswerCount}%`)
-    restart()
+    let correctAnswerCount = ((randomWordArray.length - mistakeCounter) * 100) / randomWordArray.length;
+    correctAnswerCount = Math.floor(correctAnswerCount);
+    alert(`Mistake: ${mistakeCounter}\nCorrect: ${correctAnswerCount}%`);
+    restart();
   }
 }
 
 function checkRepeat() {
-  let repeatCount = 0
-  let repeatArray = []
+  let repeatCount = 0;
+  const repeatArray = [];
   for (const word of randomWordArray) {
-    repeatCount = 0
+    repeatCount = 0;
     for (const wordCompare of randomWordArray) {
       if (word === wordCompare) {
-        repeatCount += 1
+        repeatCount += 1;
         if (repeatCount > 1) {
-          repeatArray.push(wordCompare)
+          repeatArray.push(wordCompare);
         }
       }
     }
   }
-  console.log("repeat", repeatArray)
+  console.log('repeat', repeatArray);
 }
 
 function restart() {
-  currentWord = 0
-  mistakeCounter = 0
+  currentWord = 0;
+  mistakeCounter = 0;
   if (mistakeArray.length > 0) {
-    randomWordArray = mistakeArray.slice()
-    mistakeArray = []
+    randomWordArray = mistakeArray.slice();
+    mistakeArray = [];
   } else {
-    randomWordArray = dictionary.slice()
+    randomWordArray = wordsArray.slice();
   }
-  changeWord()
+  changeWord();
 }
-
-
-
